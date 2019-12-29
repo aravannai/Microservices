1c427251661a739eff7b25fc1f37b15218319a23
aravannai <38413795+aravannai@users.noreply.github.com>
2019-12-29 12:24:45 +0530

    LimitService Changes

--- limits-service/src/main/java/com/sathish/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/sathish/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,8 +1,19 @@
 package com.sathish.microservices.limitsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sathish.microservices.limitsservice.bean.LimitsConfiguration;
 
 @RestController
 public class LimitsConfigurationController {
 
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public LimitsConfiguration retrieveLimitsFromConfiguration() {
+		return new LimitsConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
 }
