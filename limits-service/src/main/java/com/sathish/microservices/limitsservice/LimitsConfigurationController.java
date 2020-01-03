0ab791d2fe3b3878e0913fb9025da68d8543844b
aravannai <38413795+aravannai@users.noreply.github.com>
2020-01-03 07:50:11 +0530

    FaultTolerance Changes

--- limits-service/src/main/java/com/sathish/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/sathish/microservices/limitsservice/LimitsConfigurationController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.sathish.microservices.limitsservice.bean.LimitsConfiguration;
 
 @RestController
@@ -13,7 +14,18 @@ public class LimitsConfigurationController {
 	private Configuration configuration;
 	
 	@GetMapping("/limits")
+	
 	public LimitsConfiguration retrieveLimitsFromConfiguration() {
 		return new LimitsConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+	public LimitsConfiguration retrieveConfiguration() {
+		throw new RuntimeException("Not available");
+	}
+	
+	public LimitsConfiguration fallbackRetrieveConfiguration() {
+		return new LimitsConfiguration(999,9);
 	}
 }
